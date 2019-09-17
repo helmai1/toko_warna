a6ab3f2037f832c304421f4799b8e550e9d756b0
Helmi Effendi <helmiefendi64@gmail.com>
2019-09-17 12:53:23 +0700

    fix edit dan tambah pada supplier dan user

--- admin/supplier-edit.php
+++ admin/supplier-edit.php
@@ -52,14 +52,14 @@
 
     <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
       <div class="container">
-        <a class="navbar-brand" href="index.php">Edit Supplier</a>
+        <a class="navbar-brand" href="daftar-supplier.php">Edit Supplier</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav ml-auto">
-              <li class="nav-item"><a class="btn btn-outline-danger" href="index.php"><i class="fa fa-sign-out-alt"></i></a></li>
+              <li class="nav-item"><a class="btn btn-outline-danger" href="daftar-supplier.php"><i class="fa fa-sign-out-alt"></i></a></li>
             </ul>
         </div>
       </div>
